f46b3062dd5921c4778d107d6da554e81b2e73ad
Nahuel Garcia <nhlgrc@hotmail.com>
2025-04-10 17:59:06 -0300

    desde saico

--- src/main/java/org/ngarcia/webapp/ejb/controllers/EjemploServlet.java
+++ src/main/java/org/ngarcia/webapp/ejb/controllers/EjemploServlet.java
@@ -1,25 +1,48 @@
 package org.ngarcia.webapp.ejb.controllers;
 
 import jakarta.ejb.EJB;
+import jakarta.inject.Inject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 import org.ngarcia.webapp.ejb.service.ServiceEjb;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import java.io.IOException;
 
 @WebServlet("/index")
 public class EjemploServlet extends HttpServlet {
 
-   @EJB
-   private ServiceEjb service;
+   //si se inyecta con @EJB se ignora el contexto @RequestScoped, @SessionScoped, etc
+   //@EJB
+   //@Inject
+   //private ServiceEjb service;
 
-   @EJB
-   private ServiceEjb service2;
+   //@EJB
+   //@Inject
+   //private ServiceEjb service2;
 
    @Override
    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
            throws ServletException, IOException {
+
+      //si el servicio es @Stateless es siempre la misma instancia.
+      //si el servicio es @Stateful los services son estáticos pero diferentes
+      //si el servicio es @Stateful con @RequestScoped los services son iguales,
+      //pero por cada request es un par nuevo
+
+      //Equivale a los @Inyect
+      ServiceEjb service = null;
+      ServiceEjb service2 = null;
+
+      try {
+         InitialContext ctx = new InitialContext();
+         service = (ServiceEjb) ctx.lookup("java:global/webapp-ejb/ServiceEjb!org.ngarcia.webapp.ejb.service.ServiceEjb");
+         service2 = (ServiceEjb) ctx.lookup("java:global/webapp-ejb/ServiceEjb!org.ngarcia.webapp.ejb.service.ServiceEjb");
+      } catch (NamingException e) {
+         e.printStackTrace();
+      }
 
       System.out.println("service = service2:" +service.equals(service2));
       req.setAttribute("saludo",service.saludar("Carlos"));
--- src/main/java/org/ngarcia/webapp/ejb/service/ServiceEjb.java
+++ src/main/java/org/ngarcia/webapp/ejb/service/ServiceEjb.java
@@ -1,12 +1,20 @@
 package org.ngarcia.webapp.ejb.service;
 
-import jakarta.ejb.Stateless;
+import jakarta.ejb.*;
+import jakarta.enterprise.context.RequestScoped;
 
 @Stateless
+//@RequestScoped
+//@Stateful
 public class ServiceEjb {
 
    private int contador;
 
+   //si el servicio es @Stateless es siempre la misma instancia. El contador se va incrementando
+   //si el servicio es @Stateful son diferentes instancias estáticas. El contador se incrementa por
+   //cada instancia. Esto es así porque el contexto es el del Request, eso puede cambiarse
+   //si el servicio es @Stateful con @RequestScoped los services son iguales,
+   //pero por cada request es un par nuevo por lo tanto el contador siempre imprime 1 y 2
    public String saludar(String nombre) {
       contador ++;
       System.out.println("imprimiendo ejb: " + this);
